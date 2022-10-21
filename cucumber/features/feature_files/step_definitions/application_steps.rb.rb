62f1c2ce4d73702828c76a66a570c81d8a51b75c
Divyansh <dbokadia@gmail.com>
2022-10-21 10:49:05 -0500

    Cucumber tests for sign up form negative paths

---
+++ cucumber/features/feature_files/step_definitions/application_steps.rb.rb
@@ -0,0 +1,113 @@
+Given(/^I have no applicants$/) do
+  pending
+end
+
+Given(/^I have entered an valid name$/) do
+  pending
+end
+
+When(/^I click submit$/) do
+  pending
+end
+
+Then(/^I should not see any prompts$/) do
+  pending
+end
+
+Given(/^I have entered an invalid email$/) do
+  pending
+end
+
+Then(/^I should a prompt stating email is incorrect$/) do
+  pending
+end
+
+Given(/^I have entered an valid email$/) do
+  pending
+end
+
+Given(/^I have entered an invalid contact$/) do
+  pending
+end
+
+Then(/^I should a prompt stating contact is incorrect$/) do
+  pending
+end
+
+Given(/^I have entered an valid contact$/) do
+  pending
+end
+
+Given(/^I have not selected if I have a campus job$/) do
+  pending
+end
+
+Then(/^I should see a prompt stating that I should select an option$/) do
+  pending
+end
+
+Given(/^I have selected if I have a campus job$/) do
+  pending
+end
+
+Then(/^I should not see a prompt$/) do
+  pending
+end
+
+Given(/^I have not selected if I am an undergrad student$/) do
+  pending
+end
+
+Given(/^I have selected if I am an undergrad student$/) do
+  pending
+end
+
+Then(/^I shouldn't see a prompt$/) do
+  pending
+end
+
+Given(/^I have not selected any courses$/) do
+  pending
+end
+
+Then(/^I should see a prompt stating that I should select courses$/) do
+  pending
+end
+
+Given(/^I have selected the courses I have taken$/) do
+  pending
+end
+
+Given(/^I have uploaded my Resume$/) do
+  pending
+end
+
+Given(/^I have not uploaded my Resume$/) do
+  pending
+end
+
+Then(/^I should should see a prompt stating that resume is not uploaded\.$/) do
+  pending
+end
+
+Given(/^I have uploaded my Transcript$/) do
+  pending
+end
+
+Given(/^I have not uploaded my Transcipt$/) do
+  pending
+end
+
+Then(/^I should should see a prompt stating that Transcipt is not uploaded\.$/) do
+  pending
+end
+
+And(/^I am on submit application form$/) do
+  pending
+end
+
+
+
+And(/^I select "Do you have a on\-campus job"([^"]*)""$/) do |arg1, arg2|
+  pending
+end
