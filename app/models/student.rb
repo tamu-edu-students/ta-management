20305c1768069ab4475fd4421915a5c4a95536ae
Suman Kumari <sumank@tamu.edu>
2022-10-21 18:44:00 -0500

    cucumber test for TA application

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
+    validates :name, presence: true, format: { with: /\A[a-zA-Z'-]*\z/ }
+    validates :email_id, presence: true
+    validates :uin, presence: true, length: { is: 9 }, format: { with: /\A[0-9]*\z/ }
   :attr_accessor
 end
