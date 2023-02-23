c801a56c29926498fa91180fcb337d44ab79d710
0x0384 <0x0384@gmail.com>
2023-02-23 05:03:57 -0600

    refactored the csv parse script

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,26 +1,11 @@
 class SchedulesController < ApplicationController
-    require 'csv'
+    
     def import
         # binding.b
         file = params[:file]
         return redirect_to schedules_path, notice: "Only CSV file please" unless file.content_type == "text/csv" 
 
-        file = File.open(file)
-        csv = CSV.parse(file, headers: true, col_sep: ",")
-        csv.each do |row|
-            schedule_hash = {}
-            schedule_hash[:code] = row['Code']
-            schedule_hash[:course_id] = row['Course']
-            schedule_hash[:section] = row['Section']
-            schedule_hash[:lecture_day] = row['Lecture Day']
-            schedule_hash[:start_time] = row['Start Time']
-            schedule_hash[:end_time] = row['End Time']
-            schedule_hash[:professor] = row['Instructor']
-            schedule_hash[:students] = [row['PT 1'], row['PT 2']]
-            
-            # binding.b
-            Schedule.create(schedule_hash)
-        end
+        CsvImportUsersService.new.call(file)
         redirect_to admin_path, notice: "Import successful"
     end
     def index
