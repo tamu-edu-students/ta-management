959974e2a73f2ca706e7e752d524f7f2d3ba8070
0x0384 <0x0384@gmail.com>
2023-03-06 11:16:36 -0600

    added header partial

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,18 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+
     def log_out(user)
         session[:user_id] = ""
+    end
+
+    # Returns the current logged-in user (if any).
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !current_user.nil?
     end
 end
