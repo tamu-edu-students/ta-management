cad00cd8a42565b52a7e810c82c217729fd4c6e9
Ubuntu <0x0384@gmail.com>
2023-04-04 15:47:16 +0000

    Handled when admin clicks on import schedule without selecting any file

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,11 +1,15 @@
 class SchedulesController < ApplicationController
     # before_action :require_user_logged_in!
+    before_action :admin_user
     
     def import
-        # binding.b
-        file = params[:file]
-        return redirect_to schedules_path, notice: "Only CSV file please" unless file.content_type == "text/csv" 
-
+       # binding.b
+        if params[:file].nil?
+            return redirect_to schedules_path, notice: "You did not select any file. Try again"
+        else
+            file = params[:file]
+            return redirect_to schedules_path, notice: "Only CSV file please" unless file.content_type == "text/csv"   
+        end
         CsvImportUsersService.new.call(file)
         redirect_to admin_path, notice: "Successfully imported"
     end
@@ -25,4 +29,14 @@ class SchedulesController < ApplicationController
           format.json { head :no_content }
         end
     end
+    
+    private
+        
+     # Confirms an admin user
+      def admin_user
+        unless is_admin?
+          flash[:danger] = "You do not have administrative access to this page."
+          redirect_back(fallback_location: { action: "show", id: session[:user_id]})
+        end
+      end
 end
