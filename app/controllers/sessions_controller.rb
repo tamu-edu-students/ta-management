f126ca7ec2339def604b9a0c7514215194553b61
Ubuntu <0x0384@gmail.com>
2023-04-07 17:12:02 +0000

    test for schedule model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,26 +4,8 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # user = User.find_by(email_id: params[:session][:email_id].downcase)
-    # if user && user.authenticate(params[:session][:password])
-    #   if user.access_level == "admin"
-    #     log_in user
-    #     redirect_to admin_path
-    #   elsif user.access_level == "Professor"
-    #     log_in user
-    #     redirect_to professor_url(user)
-    #   else
-    #     log_in user
-    #     redirect_to user
-    #   end
-    # else
-    #   flash.now[:danger] = 'Invalid email/password combination'
-    #   render 'new', status: :unprocessable_entity
-    # end
-
     @user = User.find_by(email_id: params[:session][:email_id].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      # session[:id] = @user.id
       log_in @user
       case @user.access_level
       when 'Professor'
@@ -48,46 +30,7 @@ class SessionsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
-  # def create
-  #   user = User.new(user_params)
-  #   if user && user.authenticate(params[:session][:password])
-  #     if user.access_level == "admin"
-  #       log_in user
-  #       redirect_to admin_path
-  #     elsif user.access_level == "Professor"
-  #       log_in user
-  #       redirect_to professor_url(user)
-  #     else
-  #       log_in user
-  #       redirect_to user
-  #     end
-  #   else
-  #     flash.now[:danger] = 'Invalid email/password combination'
-  #     render 'new', status: :unprocessable_entity
-  #   end
-  # end
   
-  # def create
-  #   # @user = User.new(user_params)
-  #   # respond_to do |format|
-  #   #   if @user.save
-  #   #     log_in @user
-  #   #     if params[:access_level] == 'admin'
-  #   #       format.html { redirect_to admin_path, notice: "User was successfully created." }
-  #   #     else
-  #   #       session[:id] = @user.id
-  #   #       format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-  #   #       format.json { render :show, status: :created, location: @user }
-  #   #     end
-  #   #   else
-  #   #     flash[:alert] = @user.errors.full_messages
-  #   #     format.html { render :new, status: :unprocessable_entity }
-  #   #     format.json { render json: @user.errors, status: :unprocessable_entity }
-  #   #   end
-  #   # end
-  # end
-
   def destroy
     user = User.find(session[:user_id])
     log_out user
@@ -97,7 +40,6 @@ class SessionsController < ApplicationController
 
   private
   # Only allow a list of trusted parameters through.
-
     def user_params
       params.require(:user).permit(:name, :email_id, :password, :password_confirmation, :access_level)
     end
