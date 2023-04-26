7ddc10cccba677e97092a2a467860f3fe4ac5a9d
Ubuntu <0x0384@gmail.com>
2023-04-26 14:33:52 +0000

    changes

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,8 @@
 class SchedulesController < ApplicationController
     before_action :admin_user
     
+    @@skipped_rows = []
+    
     def import
        # binding.b
         if params[:file].nil?
@@ -9,7 +11,11 @@ class SchedulesController < ApplicationController
             file = params[:file]
             return redirect_to schedules_path, notice: "Only CSV file please" unless file.content_type == "text/csv"   
         end
-        CsvImportUsersService.new.call(file)
+        #CsvImportUsersService.new.call(file)
+        @@skipped_rows = CsvImportUsersService.new.call(file)
+        
+        #store_skipped_pts(@skipped_pts)
+        
         redirect_to admin_path, notice: "Successfully imported"
     end
     def index
@@ -18,6 +24,20 @@ class SchedulesController < ApplicationController
 
     def view
         @schedules = Schedule.all
+        @skipped_rows = @@skipped_rows
+        # @@skipped_rows
+        puts "Testing 1"
+        puts @skipped_rows.length()
+        puts "Testing"
+        
+        # if @skipped_rows.any?
+        #     skipped_rows_msg = "The following rows were not saved: "
+        #     @skipped_rows.each do |row|
+        #         # skipped_rows_msg ||= ""
+        #         skipped_rows_msg += row.inspect + "; "
+        #     end
+        #     flash[:warning] = skipped_rows_msg
+        # end
     end
 
     def destroy
@@ -29,6 +49,9 @@ class SchedulesController < ApplicationController
         end
     end
     
+    
+    # skipped_rows = import
+    # view(skipped_rows)
     private
         
      # Confirms an admin user
@@ -38,4 +61,7 @@ class SchedulesController < ApplicationController
           redirect_back(fallback_location: { action: "show", id: session[:user_id]})
         end
       end
+        def store_skipped_pts(skipped_pts)
+            return @skipped_pts
+        end
 end
